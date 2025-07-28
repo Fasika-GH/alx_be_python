d751e399f3a43863bc2fb681ce1640269e1d325e
Fasika GH <fasikagh3@gmail.com>
2025-07-28 11:54:53 +0300

    Remove variables from function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,7 +1,4 @@
-def perform_operation(num1: float, num2: float, operation: str):
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+def perform_operation(num1, num2, operation):
 
     addition = num1 + num2
     subtraction = num1 - num2
