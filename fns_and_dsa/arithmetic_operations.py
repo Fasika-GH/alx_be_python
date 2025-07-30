8234a29bdd2a34e98db034c4c7ed3109b0c10376
Fasika Girma <fasikagh3@gmail.com>
2025-07-30 19:20:56 +0300

    Organized the variables

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,21 +1,18 @@
-def perform_operation(num1: float, num2: float, operation: str):
-    # num1 = float(input("Enter the first number: "))
-    # num2 = float(input("Enter the second number: "))
-    # operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
-    addition = num1 + num2
-    subtraction = num1 - num2
-    multiplication = num1 * num2
-    division = num1/num2
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+
+def perform_operation(num1, num2, operation):
 
     if operation == "add":
-        print("The result is: ", addition)
+        return num1 + num2
     elif operation == "subtract":
-        print("The result is: " + subtraction)
+        return num1 - num2
     elif operation == "multiply":
-        print("The result is: ", multiplication)
+        return num1 * num2
     elif operation == "divide" :
-        print("The result is: ", division)
+        return num1/num2
     elif operation == "divide" and num2 == 0:
         print("The result is not defined")
     else:
