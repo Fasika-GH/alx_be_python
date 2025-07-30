8865ae514dead2bfc49f9af2864d4eaf57ad09a5
Fasika Girma <fasikagh3@gmail.com>
2025-07-30 19:37:50 +0300

    Updated arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -4,6 +4,7 @@ num2 = float(input("Enter the second number: "))
 operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
 def perform_operation(num1, num2, operation):
+   
 
     if operation == "add":
         return num1 + num2
@@ -11,11 +12,13 @@ def perform_operation(num1, num2, operation):
         return num1 - num2
     elif operation == "multiply":
         return num1 * num2
-    elif operation == "divide" :
-        print("The result is: ", division)
-    elif operation == "divide" and num2 == 0:
-        print("The result is not defined")
+    elif operation == "divide":
+        if num2 == 0:
+            return "The result is not defined (division by zero)"
+        else:
+            return num1 / num2
     else:
         print ("Input is incorrect.")
 
-perform_operation()
+result = perform_operation(num1, num2, operation)
+print("Result: ", result)
