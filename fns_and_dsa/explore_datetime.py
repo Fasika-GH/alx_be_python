8cf7ec254e581dcfef11801ad98496047d8a8278
Fasika Girma <fasikagh3@gmail.com>
2025-07-31 08:14:54 +0300

    CHange return to print on display current datetime function

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 def display_current_datetime():
     current_date = datetime.now()
     formatted = current_date.strftime("%Y-%m-%d %H:%M:%S")
-    return formatted
+    print(formatted)
 
 display_current_datetime()
 
