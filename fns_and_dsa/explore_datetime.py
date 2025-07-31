af2165917207584eae934a391b7f308c3f449505
Fasika Girma <fasikagh3@gmail.com>
2025-07-31 08:19:33 +0300

    Update number of days variable

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -8,7 +8,7 @@ def display_current_datetime():
 display_current_datetime()
 
 def calculate_future_date():
-    number_of_days = int(input("Enter number of days to add to the current date: "))
+    number_of_days = int(input("Enter number of days to add to the current date:"))
     date_today= datetime.today().date()
     future_date = date_today + timedelta(days=number_of_days)
     formatted_future_date = future_date.strftime("%Y-%m-%d")
