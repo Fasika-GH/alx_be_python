e0a6fd68fbaff5e4408c73e0e680f560a617921c
Fasika Girma <fasikagh3@gmail.com>
2025-07-31 15:10:20 +0300

    Update for loop on multiplication table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,6 @@
 number = int(input("Enter a number to see its multiplication table: "))
-factors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# factors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-for item in factors:
-    result = number * item
-    print(number, " * ", item, " = ", result)
+for factor in range(1, 10):
+    result = number * factor
+    print(number, " * ", factor, " = ", result)
