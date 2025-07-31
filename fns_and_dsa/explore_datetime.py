f897f6a1f1ebf5f77f434347b22f549caee1a018
Fasika Girma <fasikagh3@gmail.com>
2025-07-31 08:17:10 +0300

    Update calculate future date function

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -11,7 +11,8 @@ def calculate_future_date():
     number_of_days = int(input("Enter number of days: "))
     date_today= datetime.today().date()
     future_date = date_today + timedelta(days=number_of_days)
-    print ("The future date will be: ", future_date)
+    formatted_future_date = future_date.strftime("%Y-%m-%d")
+    print ("The future date will be: ", formatted_future_date)
 
 calculate_future_date()
 
