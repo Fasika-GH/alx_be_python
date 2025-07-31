8408e4ca2f40bd2baa02365f4c26f96dc9e485a8
Fasika Girma <fasikagh3@gmail.com>
2025-07-31 15:13:17 +0300

    Update for loop in multiplication table and remove factors list

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,6 @@
 number = int(input("Enter a number to see its multiplication table: "))
 # factors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-for factor in range(1, 10):
+for factor in range(1, 11):
     result = number * factor
     print(number, " * ", factor, " = ", result)
