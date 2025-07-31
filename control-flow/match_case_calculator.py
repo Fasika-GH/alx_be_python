23fcc6a9aad01f5c0ad3340006c516f60338f5cd
Fasika Girma <fasikagh3@gmail.com>
2025-07-31 14:54:47 +0300

    Update operation variable

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,15 +1,15 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-operation_type = input("Choose the operation (+, -, *, /): ")
+operation = input("Choose the operation (+, -, *, /): ")
 
-match operation_type:
-    case _ if operation_type == "+":
+match operation:
+    case _ if operation == "+":
         print("Result is: ", num1 + num2)
-    case _ if operation_type == "-":
+    case _ if operation == "-":
         print ("Result is: ", num1 - num2)
-    case _ if operation_type == "*":
+    case _ if operation == "*":
         print ("Result is : ", num1 * num2)
-    case _ if operation_type == "/":
+    case _ if operation == "/":
         print ("Result is: ", num1/num2)
 
  
